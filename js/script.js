5f4cf1eb7d65eb3544e2e2e06e1be7ec5f71d3d0
aaronmwhitehead <aaronwhitehead4@gmail.com>
2025-06-24 18:47:23 -0400

    Added image viewer

--- js/script.js
+++ js/script.js
@@ -32,15 +32,18 @@ function initMap() {
           breakpoint: 768,
           settings: {
             slidesToShow: 2,
+            slidesToScroll: 2
           }
         },
         {
           breakpoint: 1024,
           settings: {
             slidesToShow: 3,
+            slidesToScroll: 3
           }
         }
       ]
     });
   });
+  
   
